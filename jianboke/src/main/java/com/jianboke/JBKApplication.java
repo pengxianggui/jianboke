e85c228f76bcb75e01f9ff9a97e33f393b091a15
pengxianggui <pengxianggui@outlook.com>
2017-04-24 11:18:28 +0800

    增加@Audit审计功能： 创建实体和更新实体时自动更新create_date和last_modified_date字段

--- jianboke/src/main/java/com/jianboke/JBKApplication.java
+++ jianboke/src/main/java/com/jianboke/JBKApplication.java
@@ -2,8 +2,10 @@ package com.jianboke;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class JBKApplication 
 {
     public static void main( String[] args )
